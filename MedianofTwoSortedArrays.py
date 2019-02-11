20f23b4e49a1109067c48d04e7320a8f0c9e89a2
Shea Lambert <6855717+SheaML@users.noreply.github.com>
2019-02-11 14:32:29 -0500

    Create MedianofTwoSortedArrays.py

---
+++ MedianofTwoSortedArrays.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        from statistics import median
+        nums = nums1 + nums2
+        return median(nums)
