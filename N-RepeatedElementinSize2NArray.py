ca35ed3a152bc1a9c15c417a4b3777f4bc634116
Shea Lambert <6855717+SheaML@users.noreply.github.com>
2019-02-11 14:30:47 -0500

    Create N-RepeatedElementinSize2NArray.py

---
+++ N-RepeatedElementinSize2NArray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def repeatedNTimes(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        
+        for i in A:
+            if A.count(i) > 1:
+                return i
