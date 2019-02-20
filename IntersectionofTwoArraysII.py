3b3e093380f412399628d3bcfcfd51e1c5282806
Shea Lambert <6855717+SheaML@users.noreply.github.com>
2019-02-19 21:50:24 -0500

    Delete IntersectionofTwoArraysII.py

--- IntersectionofTwoArraysII.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def intersect(self, nums1: 'List[int]', nums2: 'List[int]') -> 'List[int]':
-        results=[]
-        for i in set(nums1+nums2):
-            results.extend([i]*min(nums1.count(i),nums2.count(i)))
-        return results
